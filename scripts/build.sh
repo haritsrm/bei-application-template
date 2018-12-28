a57085e9d834ef449fc37122087b90224c52d9f3
Salvian Reynaldi <5778914+salvianreynaldi@users.noreply.github.com>
2018-12-28 15:08:35 +0700

    change build script to re-enable local execution (#11)

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,9 +2,14 @@
 set -evo pipefail
 CURRENT_DIR="$(dirname "$0")"
 . ${CURRENT_DIR}/init_git.sh
-. ${CURRENT_DIR}/init_${CI_NAME}.sh
+if [ -n "${CI_NAME}" ]; then
+    . ${CURRENT_DIR}/init_${CI_NAME}.sh
+    ./gradlew build
+else
+    # local build
+    ./gradlew build --write-locks
+fi
 
-./gradlew build
 
 if [[ ${DETECTED_VERSION} =~ ${VERSION_PATTERN} ]]; then
     ./gradlew publishMavenJavaPublicationToMavenRepository -Pversion=${DETECTED_VERSION:1}
