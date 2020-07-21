3f1303a942189f5f3353efcd62b6e1fe3e45a7b8
handojo1 <3625624+handojo1@users.noreply.github.com>
2020-07-21 13:17:23 +0700

    update assume_role.sh to allow update other profiles (#37)

--- scripts/assume_role.sh
+++ scripts/assume_role.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 set -eo pipefail
 
-role_arn="arn:aws:iam::015110552125:role/BeiartfReader"
+declare -A saved_role_arn
+saved_role_arn[beiartf]="arn:aws:iam::015110552125:role/BeiartfReader"
+saved_role_arn[default]="arn:aws:iam::015110552125:role/DevExample"     # change this with the role arn that you need to run applications correctly
+
+session_name="beiartf"
 
 # parse command line
 while [ "$#" -gt 0 ]; do
     case "$1" in
         -p) profile="$2"; shift 2;;
         -r) role_arn="$2"; shift 2;;
+        -n) session_name="$2"; shift 2;;
     esac
 done
+
+if [[ -z "${role_arn}" ]]; then
+    role_arn=${saved_role_arn[${session_name}]}
+fi
 
 response=$(aws ${profile:+--profile $profile} \
                sts assume-role --output text \
@@ -23,6 +32,6 @@ AWS_SECRET_ACCESS_KEY=$(echo $response | awk '{print $3}')
 AWS_SESSION_TOKEN=$(echo $response | awk '{print $4}')
 EXPIRATION=$(echo $response | awk '{print $2}')
 
-eval "aws configure --profile beiartf set aws_access_key_id ${AWS_ACCESS_KEY_ID}"
-eval "aws configure --profile beiartf set aws_secret_access_key ${AWS_SECRET_ACCESS_KEY}"
-eval "aws configure --profile beiartf set aws_session_token ${AWS_SESSION_TOKEN}"
+eval "aws configure --profile ${session_name} set aws_access_key_id ${AWS_ACCESS_KEY_ID}"
+eval "aws configure --profile ${session_name} set aws_secret_access_key ${AWS_SECRET_ACCESS_KEY}"
+eval "aws configure --profile ${session_name} set aws_session_token ${AWS_SESSION_TOKEN}"
